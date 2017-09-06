772c35cda876bcf717cf07734977da4569cd3163
Vishwakarma, Siyaram <Siyaram.Vishwakarma@cdk.com>
2017-09-06 10:56:57 +0530

    added custom object in Set

--- src/com/corejava/CollectionSet.java
+++ src/com/corejava/CollectionSet.java
@@ -5,17 +5,26 @@ import java.util.Set;
 public class CollectionSet {
 public static void main(String[] args) {
 	TestSet ts = new TestSet();
-	Set<String> s = ts.getSet();
+	Set<Employee> s = ts.getSet();
 	System.out.println(s);
 }
 }
 
 class TestSet{	
-	Set<String> getSet() {	
-		Set<String> s = new HashSet<>();
-		s.add("1");
-		s.add("2");
-		s.add("1");
+	Set<Employee> getSet() {	
+//		Set<String> s = new HashSet<>();
+//		s.add("1");
+//		s.add("2");
+//		s.add("1");
+		Set<Employee> s = new HashSet<Employee>();
+		Employee e1 = new Employee("siyaram", "A", 100);
+		Employee e2 = new Employee("sunil", "B", 200);		
+		Employee e3 = new Employee("siyaram", "A", 100);
+		Employee e4 = new Employee("sunil", "B", 200);
+		s.add(e1);
+		s.add(e2);
+		s.add(e3);
+		s.add(e4);
 		return s;
 	}	
 }
